a94b42ecc70767f7220362454f3d653e9746bdc9
madanrnsit <154518278+madanrnsit@users.noreply.github.com>
2024-09-01 19:14:33 +0530

    Update consts.go

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "madanks"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "MADAN"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "MAD"
 )
 
 var ID ids.ID
